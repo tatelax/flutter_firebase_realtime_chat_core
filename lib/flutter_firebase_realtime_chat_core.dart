63fea7b995a8c3a893c953e1535145cbe849b547
Tate McCormick <tatelax@gmail.com>
2023-06-24 18:04:17 -0700

    Stop using local package

--- lib/flutter_firebase_realtime_chat_core.dart
+++ lib/flutter_firebase_realtime_chat_core.dart
@@ -1,5 +1,5 @@
 library flutter_firebase_realtime_chat_core;
 
-export 'src/firebase_chat_core.dart';
-export 'src/firebase_chat_core_config.dart';
+export 'src/firebase_realtime_chat_core.dart';
+export 'src/firebase_realtime_chat_core_config.dart';
 export 'src/util.dart';
